c33924cf29d71137755d86bd9c845ef0b33054a9
Pikachuz3 <tinyangel@mail.com>
2022-08-27 17:59:28 +0200

    small fixes

--- src/main/java/me/angelfire/fandomconnect/FandomConnect.java
+++ src/main/java/me/angelfire/fandomconnect/FandomConnect.java
@@ -18,54 +18,54 @@ public final class FandomConnect extends JavaPlugin {
 
 	public static FandomConnect INSTANCE;
 
-    FileConfiguration languageConfig1;
-    File languageFile1;
-    File languageFile2;
-    File languageFolder;
-    File infoboxFolder;
-    File infoboxPlayers;
-    File infoboxTowns;
-    File infoboxNations;
-    Wiki wiki;
+	FileConfiguration languageConfig1;
+	File languageFile1;
+	File languageFile2;
+	File languageFolder;
+	File infoboxFolder;
+	File infoboxPlayers;
+	File infoboxTowns;
+	File infoboxNations;
+	Wiki wiki;
 
 
 	@Override
-    public void onEnable() {
+	public void onEnable() {
 		languageFolder = new File(this.getDataFolder(), "/languages");
-        if (!languageFolder.isDirectory()){
-            languageFolder.mkdir();
-        }
+		if (!languageFolder.isDirectory()){
+			languageFolder.mkdir();
+		}
 		infoboxFolder = new File(this.getDataFolder(), "/infoboxes");
-        if (!infoboxFolder.isDirectory()){
-            infoboxFolder.mkdir();
-        }
-    	INSTANCE  = this;
-    	createFiles();
-        saveDefaultConfig();
-        FandomConnect.createListingPages(INSTANCE.getWiki(), INSTANCE.getConfig());
-        FandomConnect.createInfoboxes(INSTANCE.getWiki(), INSTANCE.getConfig());
-        getCommand("fandom").setExecutor(new Fandom());
-        if(this.getConfig().getString("data_to_send.rpcard.sync_at_restart").equals("true") && this.getConfig().getString("data_to_send.rpcard.integration_enabled").equals("true")) {
-            FileConfiguration config = YamlConfiguration.loadConfiguration(new File(FandomConnect.INSTANCE.getDataFolder(), "config.yml"));
-        	Fandom.syncPlayers(config);
-        }
-    }
+		if (!infoboxFolder.isDirectory()){
+			infoboxFolder.mkdir();
+		}
+		INSTANCE  = this;
+		createFiles();
+		saveDefaultConfig();
+		FandomConnect.createListingPages(INSTANCE.getWiki(), INSTANCE.getConfig());
+		FandomConnect.createInfoboxes(INSTANCE.getWiki(), INSTANCE.getConfig());
+		getCommand("fandom").setExecutor(new Fandom());
+		if(this.getConfig().getString("rpcard.sync_at_restart").equals("true") && this.getConfig().getString("rpcard.integration_enabled").equals("true")) {
+			FileConfiguration config = YamlConfiguration.loadConfiguration(new File(FandomConnect.INSTANCE.getDataFolder(), "config.yml"));
+			Fandom.syncPlayers(config);
+		}
+	}
 
 
 	@Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
 
 	public FileConfiguration getlanguageConfig() {
-        languageConfig1 = new YamlConfiguration();
+		languageConfig1 = new YamlConfiguration();
 		if(INSTANCE.getConfig().getString("language").equals("fr_FR")) {
-        return YamlConfiguration.loadConfiguration(languageFile2);
-        }
+			return YamlConfiguration.loadConfiguration(languageFile2);
+		}
 		else {
 			return YamlConfiguration.loadConfiguration(languageFile1);
 		}
-    }
+	}
 
 	public File getInfobox(Integer infoboxType) {
 		if (infoboxType == 1) {
@@ -82,75 +82,74 @@ public final class FandomConnect extends JavaPlugin {
 		}
 	}
 
-    private void createFiles() {
-        languageFile1 = new File(getDataFolder(), "languages/en_US.yml");
-        if (!languageFile1.exists()) {
-            languageFile1.getParentFile().mkdirs();
-            saveResource("languages/en_US.yml", false);
-         }
+	private void createFiles() {
+		languageFile1 = new File(getDataFolder(), "languages/en_US.yml");
+		if (!languageFile1.exists()) {
+			languageFile1.getParentFile().mkdirs();
+			saveResource("languages/en_US.yml", false);
+		}
 
-        languageFile2 = new File(getDataFolder(), "languages/fr_FR.yml");
-        if (!languageFile2.exists()) {
-            languageFile2.getParentFile().mkdirs();
-            saveResource("languages/fr_FR.yml", false);
-         }
+		languageFile2 = new File(getDataFolder(), "languages/fr_FR.yml");
+		if (!languageFile2.exists()) {
+			languageFile2.getParentFile().mkdirs();
+			saveResource("languages/fr_FR.yml", false);
+		}
 
-        infoboxPlayers = new File(getDataFolder(), "infoboxes/" +INSTANCE.getConfig().getString("infoboxes.players"));
-        if (!infoboxPlayers.exists()) {
-            infoboxPlayers.getParentFile().mkdirs();
-            saveResource("infoboxes/" + INSTANCE.getConfig().getString("infoboxes.players"), false);
-         }
+		infoboxPlayers = new File(getDataFolder(), "infoboxes/" +INSTANCE.getConfig().getString("infoboxes.players"));
+		if (!infoboxPlayers.exists()) {
+			infoboxPlayers.getParentFile().mkdirs();
+			saveResource("infoboxes/" + INSTANCE.getConfig().getString("infoboxes.players"), false);
+		}
 
-        infoboxTowns = new File(getDataFolder(), "infoboxes/" +INSTANCE.getConfig().getString("infoboxes.towns"));
-        if (!infoboxTowns.exists()) {
-            infoboxTowns.getParentFile().mkdirs();
-            saveResource("infoboxes/" +INSTANCE.getConfig().getString("infoboxes.towns"), false);
-         }
+		infoboxTowns = new File(getDataFolder(), "infoboxes/" +INSTANCE.getConfig().getString("infoboxes.towns"));
+		if (!infoboxTowns.exists()) {
+			infoboxTowns.getParentFile().mkdirs();
+			saveResource("infoboxes/" +INSTANCE.getConfig().getString("infoboxes.towns"), false);
+		}
 
-        infoboxNations = new File(getDataFolder(), "infoboxes/" +INSTANCE.getConfig().getString("infoboxes.nations"));
-        if (!infoboxNations.exists()) {
-            infoboxNations.getParentFile().mkdirs();
-            saveResource("infoboxes/" +INSTANCE.getConfig().getString("infoboxes.nations"), false);
-         }
-    }
+		infoboxNations = new File(getDataFolder(), "infoboxes/" +INSTANCE.getConfig().getString("infoboxes.nations"));
+		if (!infoboxNations.exists()) {
+			infoboxNations.getParentFile().mkdirs();
+			saveResource("infoboxes/" +INSTANCE.getConfig().getString("infoboxes.nations"), false);
+		}
+	}
 
-    public Wiki getWiki() {
-        FileConfiguration config = INSTANCE.getConfig();
+	public Wiki getWiki() {
+		FileConfiguration config = INSTANCE.getConfig();
 		if (config.getString("language").equals("fr_FR")) {
 			wiki = new Wiki.Builder().withApiEndpoint(HttpUrl.parse(config.getString("links.fandom") + "/fr/api.php")).build();
-			return wiki;
 		}
 		else {
 			wiki = new Wiki.Builder().withApiEndpoint(HttpUrl.parse(config.getString("links.fandom") + "/api.php")).build();
-			return wiki;
 		}
-    }
+		return wiki;
+	}
 
-    public static void createListingPages(Wiki wiki, FileConfiguration config) {
+	public static void createListingPages(Wiki wiki, FileConfiguration config) {
 		Wiki wiki2 = INSTANCE.getWiki();
 		wiki2.login(config.getString("login.username"), config.getString("login.password"));
 		FileConfiguration languageFile = INSTANCE.getlanguageConfig();
-    	if(config.getString("data_to_send.rpcard.create_page_to_list_players").equals("true")) {
-    		if (!wiki2.exists(languageFile.getString("list_management.title.players"))) {
-        		wiki2.addText(languageFile.getString("list_management.title.players"), "==" + languageFile.getString("list_management.title.players") + "==", languageFile.getString("list_management.reason.creation"), false);
-        		Bukkit.getLogger().info("[FandomConnect] Listing page for players has been created !");
-    		}
-    	}
-    	if(config.getString("data_to_send.towny.create_page_to_list_towns").equals("true")) {
-    		if (!wiki2.exists(languageFile.getString("list_management.title.towns"))) {
-        		wiki2.addText(languageFile.getString("list_management.title.towns"), "=="+ languageFile.getString("list_management.title.towns") + "==", languageFile.getString("list_management.reason.creation"), false);
-        		Bukkit.getLogger().info("[FandomConnect] Listing page for towns has been created !");
-    		}
-    	}
-    	if(config.getString("data_to_send.towny.create_page_to_list_nations").equals("true")) {
-    		if (!wiki2.exists(languageFile.getString("list_management.title.nations"))) {
-        		wiki2.addText(languageFile.getString("list_management.title.nations"), "==" + languageFile.getString("list_management.title.nations") + "==", languageFile.getString("list_management.reason.creation"), false);
-        		Bukkit.getLogger().info("[FandomConnect] Listing page for nations has been created !");
-    		}
-    	}
-    }
+		if(config.getString("rpcard.create_page_to_list_players").equals("true")) {
+			if (!wiki2.exists(languageFile.getString("list_management.title.players"))) {
+				wiki2.addText(languageFile.getString("list_management.title.players"), "==" + languageFile.getString("list_management.title.players") + "==", languageFile.getString("list_management.reason.creation"), false);
+				Bukkit.getLogger().info("[FandomConnect] Listing page for players has been created !");
+			}
+		}
+		if(config.getString("towny.create_page_to_list_towns").equals("true")) {
+			if (!wiki2.exists(languageFile.getString("list_management.title.towns"))) {
+				wiki2.addText(languageFile.getString("list_management.title.towns"), "=="+ languageFile.getString("list_management.title.towns") + "==", languageFile.getString("list_management.reason.creation"), false);
+				Bukkit.getLogger().info("[FandomConnect] Listing page for towns has been created !");
+			}
+		}
+		if(config.getString("towny.create_page_to_list_nations").equals("true")) {
+			if (!wiki2.exists(languageFile.getString("list_management.title.nations"))) {
+				wiki2.addText(languageFile.getString("list_management.title.nations"), "==" + languageFile.getString("list_management.title.nations") + "==", languageFile.getString("list_management.reason.creation"), false);
+				Bukkit.getLogger().info("[FandomConnect] Listing page for nations has been created !");
+			}
+		}
+	}
 
-    public static void createInfoboxes(Wiki wiki, FileConfiguration config) {
+	public static void createInfoboxes(Wiki wiki, FileConfiguration config) {
 		Wiki wiki2 = INSTANCE.getWiki();
 		wiki2.login(config.getString("login.username"), config.getString("login.password"));
 		FileConfiguration languageFile = INSTANCE.getlanguageConfig();
@@ -161,23 +160,23 @@ public final class FandomConnect extends JavaPlugin {
 		else {
 			modele_base_name = languageFile.getString("list_management.title.infobox_players").replaceAll("Template:", "");
 		}
-    	if(config.getString("data_to_send.rpcard.create_infobox_for_players").equals("true")) {
-    		if (!wiki2.exists(languageFile.getString("list_management.title.infobox_players"))) {
-    				wiki2.addText(languageFile.getString("list_management.title.infobox_players"), FileUtils.loadContent(FandomConnect.INSTANCE.getInfobox(1)), languageFile.getString("list_management.reason.creation"), false);
-    				Bukkit.getLogger().info("[FandomConnect] Infobox for players has been created !");
-    		}
-    	}
-    	if(config.getString("data_to_send.towny.create_infobox_for_towns").equals("true")) {
-    		if (!wiki2.exists(languageFile.getString("list_management.title.infobox_towns"))) {
-    				wiki2.addText(languageFile.getString("list_management.title.infobox_towns"), "{{" + modele_base_name + languageFile.getString("infobox.players"), languageFile.getString("list_management.reason.creation"), false);
-    				Bukkit.getLogger().info("[FandomConnect] Infobox for towns has been created !");
-				}
-    	}
-    	if(config.getString("data_to_send.towny.create_infobox_for_nations").equals("true")) {
-    		if (!wiki2.exists(languageFile.getString("list_management.title.infobox_nations"))) {
-    				wiki2.addText(languageFile.getString("list_management.title.infobox_nations"), "{{" + modele_base_name + languageFile.getString("nations.nations.custom"), languageFile.getString("list_management.reason.creation"), false);
-    				Bukkit.getLogger().info("[FandomConnect] Infobox for nations has been created !");
-    		}
-    	}
-    }
+		if(config.getString("rpcard.create_infobox_for_players").equals("true")) {
+			if (!wiki2.exists(languageFile.getString("list_management.title.infobox_players"))) {
+				wiki2.addText(languageFile.getString("list_management.title.infobox_players"), FileUtils.loadContent(FandomConnect.INSTANCE.getInfobox(1)), languageFile.getString("list_management.reason.creation"), false);
+				Bukkit.getLogger().info("[FandomConnect] Infobox for players has been created !");
+			}
+		}
+		if(config.getString("towny.create_infobox_for_towns").equals("true")) {
+			if (!wiki2.exists(languageFile.getString("list_management.title.infobox_towns"))) {
+				wiki2.addText(languageFile.getString("list_management.title.infobox_towns"), "{{" + modele_base_name + languageFile.getString("infobox.players"), languageFile.getString("list_management.reason.creation"), false);
+				Bukkit.getLogger().info("[FandomConnect] Infobox for towns has been created !");
+			}
+		}
+		if(config.getString("towny.create_infobox_for_nations").equals("true")) {
+			if (!wiki2.exists(languageFile.getString("list_management.title.infobox_nations"))) {
+				wiki2.addText(languageFile.getString("list_management.title.infobox_nations"), "{{" + modele_base_name + languageFile.getString("nations.nations.custom"), languageFile.getString("list_management.reason.creation"), false);
+				Bukkit.getLogger().info("[FandomConnect] Infobox for nations has been created !");
+			}
+		}
+	}
 }
